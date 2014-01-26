55c2b5da38c231e481753988c0cee1ca4670ae5b
mytory <mytory@gmail.com>
2014-01-26 16:30:36 +0900

    파일이 밖에서 변경됐을 때 새로고침.

--- edit.php
+++ edit.php
@@ -5,6 +5,7 @@ $content = file_get_contents($parsed['real_full_file']);
 <div class="msg"></div>
 <div class="msg2"></div>
 <script src="lib/EpicEditor/js/epiceditor.min.js"></script>
+<script src="lib/md5.js"></script>
 <script>
 var full_file = '<?php echo $parsed['full_file'] ?>';
 var epic = new EpicEditor({
@@ -22,14 +23,22 @@ var epic = new EpicEditor({
 	}
 }).load();
 
+var content_saved_md5 = CryptoJS.MD5(epic.getFiles()[full_file].content).toString();
+
 function auto_save(){
+    var content = epic.getFiles()[full_file].content;
 	$.post('save.php', {
-		content: epic.getFiles()[full_file].content,
+        content_saved_md5: content_saved_md5,
+		content: content,
 		path: '<?php echo $_GET['path'] ?>'
 	}, function(data){
 		if(data.code == 'fail'){
 			$('.msg').text(data.msg);
+        }else if(data.code == 'file changed'){
+            $('body').html('<h1 style="color: white; padding-top: 100px; text-align: center">파일이 밖에서 변경됐습니다.</h1>');
+            location.reload();
 		}else{
+            content_saved_md5 = data.content_saved_md5;
 			$('.msg').text(new Date().toString() + ' - 저장');
 		}
 	}, 'json');
--- save.php
+++ save.php
@@ -4,6 +4,21 @@ include 'functions.php';
 
 $parsed = parse_path();
 
+$current_file_content = file_get_contents($parsed['real_full_file']);
+$current_md5 = md5($current_file_content);
+$content_saved_md5 = $_REQUEST['content_saved_md5'];
+
+if($current_md5 != $content_saved_md5){
+	echo json_encode(array(
+		'current_md5' => $current_md5,
+		'content_saved_md5' => $content_saved_md5,
+		'code' => 'file changed',
+		'msg' => '파일이 밖에서 변경됐습니다.',
+		'current_file_content' => $current_file_content,
+	));
+	exit;
+}
+
 $handle = fopen($parsed['real_full_file'], 'w');
 if( ! $handle){
 	echo json_encode(array(
@@ -26,5 +41,6 @@ if( ! fwrite($handle, $_POST['content'])){
 fclose($handle);
 
 echo json_encode(array(
-	'code' => 'success'
+	'code' => 'success',
+	'content_saved_md5' => md5($_POST['content']),
 ));
